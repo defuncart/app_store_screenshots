824d32d5c367ecbc3540a9176158fb045bbb2747
James Leahy <defuncart@gmail.com>
2023-12-13 18:46:12 +0100

    Add desktop

--- lib/src/models.dart
+++ lib/src/models.dart
@@ -5,27 +5,42 @@ import 'package:flutter_test/flutter_test.dart';
 enum AppStoreDeviceType {
   androidPortrait,
   androidLandscape,
+  linux,
+  macOS,
+  windows,
 }
 
 extension AppStoreDeviceTypeExtensions on AppStoreDeviceType {
   Size get size => switch (this) {
         AppStoreDeviceType.androidPortrait => const Size(1080, 1920),
         AppStoreDeviceType.androidLandscape => const Size(1920, 1080),
+        AppStoreDeviceType.linux => const Size(1920, 1080),
+        AppStoreDeviceType.macOS => const Size(1920, 1080),
+        AppStoreDeviceType.windows => const Size(1920, 1080),
       };
 
   DeviceInfo get frame => switch (this) {
         AppStoreDeviceType.androidPortrait => Devices.android.onePlus8Pro,
         AppStoreDeviceType.androidLandscape => Devices.android.onePlus8Pro,
+        AppStoreDeviceType.linux => Devices.linux.laptop,
+        AppStoreDeviceType.macOS => Devices.macOS.macBookPro,
+        AppStoreDeviceType.windows => Devices.windows.laptop,
       };
 
   TargetPlatform get platform => switch (this) {
         AppStoreDeviceType.androidPortrait => TargetPlatform.android,
         AppStoreDeviceType.androidLandscape => TargetPlatform.android,
+        AppStoreDeviceType.linux => TargetPlatform.linux,
+        AppStoreDeviceType.macOS => TargetPlatform.macOS,
+        AppStoreDeviceType.windows => TargetPlatform.windows,
       };
 
   Orientation get orientation => switch (this) {
         AppStoreDeviceType.androidPortrait => Orientation.portrait,
         AppStoreDeviceType.androidLandscape => Orientation.landscape,
+        AppStoreDeviceType.linux => Orientation.landscape,
+        AppStoreDeviceType.macOS => Orientation.landscape,
+        AppStoreDeviceType.windows => Orientation.landscape,
       };
 }
 
