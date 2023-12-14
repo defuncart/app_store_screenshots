16af7844ffeffc54de48a90d0d73c0098b9400c6
James Leahy <defuncart@gmail.com>
2023-12-14 19:47:35 +0100

    Extract internal methods to generate_screenshots.internal.dart

--- lib/src/generate_screenshots.dart
+++ lib/src/generate_screenshots.dart
@@ -1,14 +1,14 @@
-import 'package:device_frame/device_frame.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:meta/meta.dart';
 import 'package:path/path.dart' as p;
 
 import 'common.dart';
+import 'generate_screenshots.internal.dart';
 import 'models.dart';
 import 'models.internal.dart';
 
+/// Generates [screen] number of screenshots with a given [config]
 @isTest
 void generateAppStoreScreenshots({
   VoidCallback? onSetUp,
@@ -62,94 +62,4 @@ void generateAppStoreScreenshots({
     },
     skip: skip,
   );
-}
-
-@visibleForTesting
-Widget createScreenContents({
-  required ScreenBuilder onBuildScreen,
-  ScreenWrapper? wrapper,
-  required Locale locale,
-  required TargetPlatform platform,
-  ThemeData? theme,
-  TextStyle? textStyle,
-  Iterable<LocalizationsDelegate<dynamic>>? localizationsDelegates,
-  Iterable<Locale>? supportedLocales,
-}) {
-  final widget = MaterialApp(
-    debugShowCheckedModeBanner: false,
-    localizationsDelegates: localizationsDelegates,
-    supportedLocales: supportedLocales ?? const [Locale('en')],
-    locale: locale,
-    theme: theme?.copyWith(platform: platform),
-    home: Material(child: onBuildScreen()),
-  );
-
-  return wrapper != null ? wrapper(widget) : widget;
-}
-
-@visibleForTesting
-Widget createScreenshot({
-  required ScreenshotBackground background,
-  String? text,
-  required Widget screenContents,
-  required DeviceInfo deviceFrame,
-  required bool isFrameVisible,
-  required Orientation orientation,
-  required double height,
-  TextStyle? textStyle,
-}) =>
-    SizedBox(
-      height: height,
-      child: Stack(
-        alignment: Alignment.topCenter,
-        children: [
-          // Background
-          SizedBox.expand(
-            child: background.widget,
-          ),
-          // Content
-          Padding(
-            padding: const EdgeInsets.all(48),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                if (text != null) ...[
-                  Text(
-                    text,
-                    style: textStyle,
-                  ),
-                  const SizedBox(height: 16),
-                ],
-                Expanded(
-                  child: DeviceFrame(
-                    device: deviceFrame,
-                    isFrameVisible: isFrameVisible,
-                    orientation: orientation,
-                    screen: screenContents,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-
-@visibleForTesting
-Future<void> takeScreenshot({
-  required WidgetTester tester,
-  required Widget widget,
-  PostPumpCallback? onPostPumped,
-  required String name,
-  required Size size,
-}) async {
-  await tester.pumpWidgetBuilder(
-    widget,
-    surfaceSize: size,
-  );
-  if (onPostPumped != null) {
-    await onPostPumped(tester);
-  }
-  await screenMatchesGolden(tester, name);
 }
