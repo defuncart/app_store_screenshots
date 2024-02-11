a64bb5017b279b7d48bd8e4a04f35a07b42359d6
James Leahy <defuncart@gmail.com>
2024-02-11 11:05:10 +0100

    generate_steam_assets add example

---
+++ example/test/app_store_screenshots/generate_steam_assets_test.dart
@@ -0,0 +1,58 @@
+import 'package:app_store_screenshots/app_store_screenshots.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  final theme = ThemeData.light();
+
+  generateStreamIcon(
+    onBuildIcon: (size) => Theme(
+      data: theme,
+      child: SteamAsset(
+        size: size,
+      ),
+    ),
+  );
+
+  generateStreamCover(
+    onBuildCover: (size) => Theme(
+      data: theme,
+      child: SteamAsset(
+        size: size,
+      ),
+    ),
+  );
+
+  generateStreamBackground(
+    onBuildBackground: (size) => Theme(
+      data: theme,
+      child: SteamAsset(
+        size: size,
+      ),
+    ),
+  );
+}
+
+class SteamAsset extends StatelessWidget {
+  const SteamAsset({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size.width,
+      height: size.height,
+      color: Theme.of(context).scaffoldBackgroundColor,
+      child: Center(
+        child: Icon(
+          Icons.flutter_dash,
+          color: Theme.of(context).iconTheme.color,
+          size: size.shortestSide * 0.6,
+        ),
+      ),
+    );
+  }
+}
